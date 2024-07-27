505018bd08106533c563058365178d7814b28fc2
Ryan Chen <ryan917rc@gmail.com>
2024-07-27 17:11:55 -0400

    added expensesinput

--- empower-hacks/components/ExpensesInput/ExpensesInput.jsx
+++ empower-hacks/components/ExpensesInput/ExpensesInput.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
+import './ExpensesInput.css'
 export default function ExpensesInput() {
   return (
-    <div>ExpensesInput</div>
+    <div className='expenses-input-container'>
+    <form className='expenses-input-form'>
+
+        <label className='title-label'>Add a new expense</label>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            <div style={{display: "flex", flexDirection: "column", width: "30%"}}>
+                <label className='input-label'> Select a Budget</label>
+                <select className='expenses-dropdown'>
+                    <option>Test</option>
+                </select>
+            </div>
+
+            <div style={{display: "flex", flexDirection: "column", width: "65%"}}>
+                <label className='input-label '>Amount</label>
+                <input type='number' placeholder='$5' className='expenses-input'></input>
+            </div>
+        </div>
+        <label className='input-label'>Expense Name</label>
+        <input type='text' placeholder='e.g. 7/27/2024: Coffee' className='budget-input'></input>
+
+
+        <button className='expenses-input-button'>Submit</button>
+    </form> 
+</div>
   )
 }
--- empower-hacks/src/App.jsx
+++ empower-hacks/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BudgetInput from '../components/BudgetInput/BudgetInput'
+import ExpensesInput from '../components/ExpensesInput/ExpensesInput'
 
 function App() {
 
   return (
-    <BudgetInput />
+    <div className='forms-container'>
+      <BudgetInput />
+      <ExpensesInput />
+    </div>
+    
   )
 }
 
