a86696a93b6c5b55471152a6fb8e50ba55c77606
Ryan Chen <ryan917rc@gmail.com>
2024-07-27 16:05:30 -0400

    added some styling for budget input and a submit button

--- empower-hacks/components/BudgetInput/BudgetInput.jsx
+++ empower-hacks/components/BudgetInput/BudgetInput.jsx
@@ -6,10 +6,11 @@ export default function BudgetInput() {
     <div className='budget-input-container'>
         <form className='budget-input-form'>
             <label className='title-label'>Create Budget</label>
-            <label>Budget Name</label>
-            <input type='text' placeholder='e.g. Groceries'></input>
-            <label>Amount</label>
-            <input type='number' placeholder='$100'></input>
+            <label className='input-label'>Budget Name</label>
+            <input type='text' placeholder='e.g. Groceries' className='budget-input'></input>
+            <label className='input-label '>Amount</label>
+            <input type='number' placeholder='$100' className='budget-input'></input>
+            <button className='budget-input-button'>Submit</button>
         </form> 
     </div>
 
