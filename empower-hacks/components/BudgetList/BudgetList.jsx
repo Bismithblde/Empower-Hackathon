37ba1c282d18fe41827296f28c4c683d1f5d0256
Ryan Chen <ryan917rc@gmail.com>
2024-07-29 17:29:41 -0400

    added some transitions

--- empower-hacks/components/BudgetList/BudgetList.jsx
+++ empower-hacks/components/BudgetList/BudgetList.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import BudgetCard from '../BudgetCard/BudgetCard';
 import './BudgetList.css';
-import { v4 as uuidv4 } from 'uuid';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const BudgetList = ({ budgetsArray, setBudgetArray }) => {
   return (
     <div className='budget-list-container'>
-      {budgetsArray && budgetsArray.map((budget, index) => (
-        <div key={uuidv4()} className='budget-card-container'>
-          <BudgetCard budget={budget} setBudgetArray={setBudgetArray} budgetArray={budgetsArray}/>
-        </div>
-      ))}
+      <TransitionGroup component={null}>
+        {budgetsArray && budgetsArray.map((budget) => (
+          <CSSTransition key={budget.id} timeout={500} classNames="fade">
+            <div className='budget-card-container'>
+              <BudgetCard budget={budget} setBudgetArray={setBudgetArray} budgetArray={budgetsArray}/>
+            </div>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
