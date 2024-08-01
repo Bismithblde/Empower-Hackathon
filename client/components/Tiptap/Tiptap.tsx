5427173ee9ee5856cc3e2d72999f7f248fcc9a26
Ryan Chen <ryan917rc@gmail.com>
2024-08-01 12:17:15 -0400

    Added TipTap text editor using the mui-tiptap library -- used demo code -- save button can push data into mongodb

---
+++ client/components/Tiptap/Tiptap.tsx
@@ -0,0 +1,54 @@
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+  useMediaQuery,
+  type PaletteMode,
+} from "@mui/material";
+import { useCallback, useMemo, useState } from "react";
+import Editor from "./Editor";
+
+export default function Tiptap() {
+  const systemSettingsPrefersDarkMode = useMediaQuery(
+    "(prefers-color-scheme: dark)"
+  );
+  const [paletteMode, setPaletteMode] = useState<PaletteMode>(
+    "light"
+  );
+  const togglePaletteMode = useCallback(
+    () =>
+      setPaletteMode((prevMode) => (prevMode === "light" ? "dark" : "light")),
+    []
+  );
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: paletteMode,
+          secondary: {
+            main: "#42B81A",
+          },
+        },
+      }),
+    [paletteMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+
+
+      <Box sx={{ p: 3, maxWidth: 1800, margin: "0 auto", backgroundColor: "white" }}>
+        <Editor />
+      </Box>
+    </ThemeProvider>
+  );
+}
--- client/src/pages/BlogCreator/BlogCreator.jsx
+++ client/src/pages/BlogCreator/BlogCreator.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./BlogCreator.css"
-import Tiptap from '../../../components/Tiptap/Tiptap'
+import Tiptap from '../../../components/Tiptap/Tiptap.tsx'
 export default function BlogCreator() {
   return (
     <div className='blog-creator-page-container'>
