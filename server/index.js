525362d33e2a9696dad3fa70b5db7ad6ab1373d3
Ryan Chen <ryan917rc@gmail.com>
2024-08-02 11:48:26 -0400

    fixed vercel (2nd time?)

--- server/index.js
+++ server/index.js
@@ -1,33 +1,33 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express()
-const userRouter = require('./routes/user')
-const blogcreatorRouter = require('./routes/blogcreator')
-const achievementRouter = require("./routes/achievement")
+const app = express();
+const userRouter = require('./routes/user');
+const blogcreatorRouter = require('./routes/blogcreator');
+const achievementRouter = require('./routes/achievement');
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Use the PORT from the .env file
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
-app.use("/api", userRouter)
-app.use("/api", blogcreatorRouter)
-app.use("/api", achievementRouter)
-app.get("/api", (req, res) => {
-    res.json({"Test": ["Test1"]})
-})
+app.use('/api', userRouter);
+app.use('/api', blogcreatorRouter);
+app.use('/api', achievementRouter);
+app.get('/api', (req, res) => {
+    res.json({ "Test": ["Test1"] });
+});
 
 // Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server started on port ${PORT}`)
-        })
+        console.log(`Connected to MongoDB`);
     })
     .catch((error) => {
-        console.error('Database connection error:', error)
-    })
+        console.error('Database connection error:', error);
+    });
+
+module.exports = app;
